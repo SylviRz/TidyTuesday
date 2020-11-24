eebbf27031c5067f3668dd58bc8701dc6d3dce21
SylviRz <52199097+SylviRz@users.noreply.github.com>
2020-11-24 22:12:51 +0100

    TidyTuesdayGraph Tuesday Version

--- 20201124.R
+++ 20201124.R
@@ -11,3 +11,107 @@ library(ggplot2)
 library(viridis)
 library(hrbrthemes)
 library(tidyverse)
+library(purrr)
+
+tuesdata <- tidytuesdayR::tt_load('2020-11-24')
+
+hike_data <- tuesdata$hike_data
+
+#hike_data <- hike_data %>% 
+#  mutate(number_features = purrr::map_int(features, length))
+
+hike_data_clean<- hike_data %>%
+  tidyr::unnest(features, keep_empty=TRUE) %>%
+  group_by(name, location) %>%
+  mutate(number_features=n()) %>%
+  ungroup() %>%
+  group_by(as.factor(features)) %>%
+  mutate(n=n()) %>%
+  ungroup()
+
+#Short data 2
+hike_data_short2<-hike_data_clean%>%
+  select(features, n) %>%
+  pivot_wider()
+
+#trying to shuffle the data a bit differently... !? TO Do!
+hike_data_short3 <- hike_data_short2[sample(nrow(hike_data_short2)),]
+
+
+#Short data. 
+hike_data_short<-hike_data_clean %>%
+  select(name, location, rating, number_features) %>%
+  mutate(rating=as.numeric(rating)) %>%
+  filter(number_features<10) %>%
+  pivot_wider()
+
+summary(hike_data_clean)
+
+table(hike_data_clean$features)
+
+
+
+plot1<-ggplot(hike_data_clean, aes(x=fct_infreq(features))) + #learning: fct_infreq(features)
+  geom_bar(stat="count") +
+  coord_flip( )
+plot1
+
+#This will be the final plot
+plot4<-ggplot(hike_data_short3, aes(x=as.numeric(as.factor(features)), y=n)) +
+  geom_area( fill="bisque3") +
+  geom_line(color="bisque4", size=2) +
+  geom_point(size=3, color="bisque4") +
+  scale_x_discrete(guide = guide_axis(n.dodge = 3)) +
+# edit the theme
+theme(text = element_text(family = "Andale Mono"), legend.position = "none", # change all text font and move the legend to the bottom
+      panel.grid = element_line(color="white"),  # change the grid color and remove minor y axis lines
+      plot.caption = element_text(hjust = 0.5, size = 8, color = "#29AF7FFF"), # remove x-axis text and edit the caption (centered and brown)
+      panel.grid.major = element_blank(),
+      panel.background = element_rect(fill="lightblue"),
+      #strip.background = element_rect(colour="white", fill="#FFFFFF"), # removing the gray facet_wrap box
+      plot.title = element_text(size = 24), plot.subtitle = element_markdown(size=9, family = "Helvetica", color = "#29AF7FFF")) +# make the title bigger and edit the subtitle (font)
+  # title
+  labs(title = "On a mountain high",
+       subtitle = "Mountain Views are the most common feature on Washington Trails. \\ Followed by: hiking with your dog on a leash, and Wildflowers.",
+       x = "Most frequent features of hikes", y=NULL,
+       caption = "DataViz by @SylviRz for #TidyTuesday, data from Washington Trails Association")
+plot4
+ggsave("mountainhigh1.png")
+
+# to do:
+#   - subtitle line break
+# - order the hike features a bit differently
+# - include features as axis labels for the numeric var...
+
+
+
+plot3<-ggplot(hike_data_clean, aes(x=features)) + #learning: fct_infreq(features)
+  geom_density() 
+plot3
+
+plot2<-ggplot(hike_data_short, aes(y=rating, x=number_features)) +
+                geom_count() +
+  geom_smooth()
++
+  # edit the theme
+  theme(text = element_text(family = "Andale Mono"), legend.position = "none", # change all text font and move the legend to the bottom
+        panel.grid = element_line(color="white"),  # change the grid color and remove minor y axis lines
+        plot.caption = element_text(hjust = 0.5, size = 8, color = "#29AF7FFF"), # remove x-axis text and edit the caption (centered and brown)
+        strip.background = element_rect(colour="white", fill="#FFFFFF"), # removing the gray facet_wrap box
+        plot.title = element_text(size = 24), plot.subtitle = element_markdown(size=9, family = "Helvetica", color = "#29AF7FFF")) +# make the title bigger and edit the subtitle (font)
+  # title
+  labs(title = "Call me mobile",
+       subtitle = "In 2016, 30% of Africans and 90% of Europeans have at least one mobile phone subscription.  ",
+       x = "Number of subscriptions per person", y=NULL,
+       caption = "data from OurWorldInData.org")
+plot2
+
+#notion app 
+#workflow 
+
+scale_colour_gradientn()
+
+Von Andreas an Alle: (9:59 PM)
+https://www.datanovia.com/en/blog/ggplot-legend-title-position-and-labels/
+# the code for getting length in numeric, found it on the GitHub page of tidytuesday
+>as.numeric(gsub("(\\d+[.]\\d+).*","\\1", hike_data$length))
