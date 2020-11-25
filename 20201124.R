1bb60d5ecc5d01a41c8e0bdcd8f12827d78c33c5
SylviRz <52199097+SylviRz@users.noreply.github.com>
2020-11-25 20:30:04 +0100

    final code and plot for tt 24/11/2020

--- 20201124.R
+++ 20201124.R
@@ -21,7 +21,46 @@ hike_data <- tuesdata$hike_data
 #  mutate(number_features = purrr::map_int(features, length))
 
 hike_data_clean<- hike_data %>%
-  tidyr::unnest(features, keep_empty=TRUE) %>%
+  tidyr::unnest(features) 
+
+
+#This will be the final plot
+plot4<-ggplot(hike_data_clean, aes(x=fct_rev(fct_infreq(features)))) + # ordered ascendingly
+   geom_bar(stat="count", fill="bisque3", color="bisque4") +
+  # Highlighting just a couple of features
+   scale_x_discrete(labels=c("Coast", "", "", "", "", "", "Ridges/Passes", "", "","", "","","", "", "Mountain \n Views")) +
+  # edit the theme
+  theme(text = element_text(family = "Andale Mono"), legend.position = "none", # change all text font and move the legend to the bottom
+        panel.grid = element_line(color="white"),  # change the grid color and remove minor y axis lines
+        plot.caption = element_text(hjust = 0.5, size = 8, color = "bisque4"), # remove x-axis text and edit the caption (centered and brown)
+        panel.grid.major = element_blank(),
+        panel.background = element_rect(fill="lightblue"),
+        #strip.background = element_rect(colour="white", fill="#FFFFFF"), # removing the gray facet_wrap box
+        plot.title = element_text(size = 24), 
+        plot.subtitle = element_markdown(size=8, family = "Helvetica", color = "bisque4")) + # make the title bigger and edit the subtitle (font)
+  # title
+  labs(title = "On a mountain high",
+       subtitle = "Mountain Views, dogs on a leash allowed, and Wildflowers top 3 features of Washington Trails.", 
+       x = "Most frequent features of hikes", y=NULL,
+       caption = "DataViz by @SylviRz for #TidyTuesday, Data: Washington Trails Association")
+plot4
+ggsave("mountainhigh1.png")
+
+# to do:
+#   - subtitle line break
+# - order the hike features a bit differently OK
+# - include features as axis labels for the numeric var... differnt---
+
+# 3 learnings
+ #scale_x_discrete(guide = guide_axis(n.dodge = 3)) -- to have the xaxis labels spread out on 2 rows
+ #tidyr::unnest(features, keep_empty=TRUE) -- to turn a list into a variable, 
+    #option keep_empty has FALSE as default, make sure to put keep_empty=TRUE
+ # within ggplot use fct_infreq() to order a factor from most common to least common, place fct_rev() to order ascendingly
+
+#old:
+hike_data_clean<- hike_data %>%
+  tidyr::unnest(features, keep_empty=TRUE) 
+%>%
   group_by(name, location) %>%
   mutate(number_features=n()) %>%
   ungroup() %>%
@@ -35,7 +74,9 @@ hike_data_short2<-hike_data_clean%>%
   pivot_wider()
 
 #trying to shuffle the data a bit differently... !? TO Do!
-hike_data_short3 <- hike_data_short2[sample(nrow(hike_data_short2)),]
+set.seed(543)
+roworder<-sample(nrow(hike_data_short2))
+hike_data_short3 <- hike_data_short2[roworder,]
 
 
 #Short data. 
@@ -55,33 +96,6 @@ plot1<-ggplot(hike_data_clean, aes(x=fct_infreq(features))) + #learning: fct_inf
   geom_bar(stat="count") +
   coord_flip( )
 plot1
-
-#This will be the final plot
-plot4<-ggplot(hike_data_short3, aes(x=as.numeric(as.factor(features)), y=n)) +
-  geom_area( fill="bisque3") +
-  geom_line(color="bisque4", size=2) +
-  geom_point(size=3, color="bisque4") +
-  scale_x_discrete(guide = guide_axis(n.dodge = 3)) +
-# edit the theme
-theme(text = element_text(family = "Andale Mono"), legend.position = "none", # change all text font and move the legend to the bottom
-      panel.grid = element_line(color="white"),  # change the grid color and remove minor y axis lines
-      plot.caption = element_text(hjust = 0.5, size = 8, color = "#29AF7FFF"), # remove x-axis text and edit the caption (centered and brown)
-      panel.grid.major = element_blank(),
-      panel.background = element_rect(fill="lightblue"),
-      #strip.background = element_rect(colour="white", fill="#FFFFFF"), # removing the gray facet_wrap box
-      plot.title = element_text(size = 24), plot.subtitle = element_markdown(size=9, family = "Helvetica", color = "#29AF7FFF")) +# make the title bigger and edit the subtitle (font)
-  # title
-  labs(title = "On a mountain high",
-       subtitle = "Mountain Views are the most common feature on Washington Trails. \\ Followed by: hiking with your dog on a leash, and Wildflowers.",
-       x = "Most frequent features of hikes", y=NULL,
-       caption = "DataViz by @SylviRz for #TidyTuesday, data from Washington Trails Association")
-plot4
-ggsave("mountainhigh1.png")
-
-# to do:
-#   - subtitle line break
-# - order the hike features a bit differently
-# - include features as axis labels for the numeric var...
 
 
 
